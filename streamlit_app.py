8385e8bc76e6981c7ed0ff89d0895053b70e927b
CharlesR-Eaton <charlesrandall@eaton.com>
2024-07-04 10:49:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Select Box
 cnx = st.connection("snowflake")
 session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
